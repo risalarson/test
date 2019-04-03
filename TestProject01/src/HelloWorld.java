7d3dce094bf34404aaeef2038a7c42ac5f845a6c
ayano-fujioka <fumi20somaru@gmail.com>
2019-04-03 14:05:37 +0900

    2019/4/3

--- TestProject01/src/HelloWorld.java
+++ TestProject01/src/HelloWorld.java
@@ -2,10 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		String b ="山田";
-		if(b.equals("山田")) {
-			System.out.println(b+"さん");
-		}
+		int i=1;
+		do{
+			System.out.println(i);
+			i+=2;
+		}while(i<=10);
 	}
-
 }
